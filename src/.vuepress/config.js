a4d955e0419bda936f675cfc622c5bb7899ca583
zuramai <ahmadsaugigis@yahoo.com>
2021-08-29 07:21:46 +0700

    init: docs

---
+++ src/.vuepress/config.js
@@ -0,0 +1,67 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Vue Next Datatable',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/devoverid/vue-next-datatable',
+    editLinks: true,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Documentation',
+        link: '/introduction/',
+      },
+      {
+        text: 'Config',
+        link: '/config/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/devoverid/vue-next-datatable',
+      },
+    ],
+    sidebar: {
+      '/introduction/': [
+        {
+          title: 'Introduction',
+          collapsable: false,
+          children: ['', 'basic-example'],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: a Lightweight Datatable for Vue 3
+actionText: Quick Start →
+actionLink: /introduction/
+features:
+- title: Lightweight
+  details: Feature 1 Description
+- title: Plugin System
+  details: Feature 2 Description
+- title: Integrated With Server-Side
+  details: Feature 3 Description
+footer: Made with ❤️ by DevoverID Community
+---
