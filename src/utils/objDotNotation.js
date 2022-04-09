5e96b7d622662cae8e9e8b79331144858d27173d
Fauzan "Evan Edrok" 0x <fncolon@pm.me>
2022-04-10 03:48:53 +0700

    ref: using `reduce` for dotNotation

--- src/utils/objDotNotation.js
+++ src/utils/objDotNotation.js
@@ -1,9 +1,3 @@
 export const objFromDotNotation = (obj, notation) => {
-  if (!`${notation}`.includes('.')) return obj[notation]
-  let value = undefined
-  const dots = notation.split('.')
-  for (let i = 0; i < dots.length; i++) {
-    value = value === undefined ? obj[dots[i]] : value[dots[i]]
-  }
-  return value
+  return notation.split(".").reduce((o, i) => o?.[i], obj);
 }
